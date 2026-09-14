13d62da7a691e41bf756ccc072e18865aac7a9e0
Chip-475 <gioelegarozzo2008@gmail.com>
2026-09-14 16:46:15 +0200

    cells spawning

--- Base Builder/Assets/Scripts/World.cs
+++ Base Builder/Assets/Scripts/World.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 
 public class World
 {
-    public World(Vector2Int size)
+    public World(Grid grid)
     {
-        cells = new Cell[size.x, size.y];
+        foreach (Tilemap tilemap in grid.GetComponentsInChildren<Tilemap>())
+            foreach (Vector3Int pos in tilemap.cellBounds.allPositionsWithin)
+            {
+                if (!tilemap.HasTile(pos))
+                    continue;
+
+                Vector2Int coord = pos.ToVector2Int();
+                cells[coord] = new(coord, CellType.Floor, true);
+            }
     }
 
-    Cell[,] cells;
+    Dictionary<Vector2Int, Cell> cells = new();
 }
--- Base Builder/Assets/Scripts/WorldManager.cs
+++ Base Builder/Assets/Scripts/WorldManager.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class WorldManager : MonoBehaviour
 {
+    public static WorldManager instance;
+
     [SerializeField] Grid grid;
+    World world;
+
+    public Sprite floorSprite;
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+    }
+    void Start()
+    {
+        grid.gameObject.SetActive(false);
+        world = new(grid);
+    }
 }
