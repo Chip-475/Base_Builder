08aa147a8ba404178bf83930777bdb61ac40d839
mastery0 <giulio.dibartolo23@gmail.com>
2026-09-22 19:24:29 +0200

    electricity

--- Base Builder/Assets/Scripts/Machine.cs
+++ Base Builder/Assets/Scripts/Machine.cs
@@ -6,16 +6,36 @@ public class Machine : InstalledObject
     [Header("Config")]
     [SerializeField] List<RecipeSO> allowedRecipes = new();
     public List<ResourceSO> allowedFuels = new();
+    private bool isworking;
+    private bool isWorking { 
+        get { return isworking; } 
+        set
+        {
+            isworking=value;
+            if(isworking)poleManager.instance.powerConsumption+=energyConsumption;
+            else poleManager.instance.powerConsumption-=energyConsumption;
+        } 
+    }
     public bool needsEnergy = true;
-    public int currentEnergy = 100;
-    public int maxEnergy = 100;
+    public int energyConsumption = 1;//to decide if energyConsuption is per machine or per recipe
+    /*[HideInInspector]*/public bool isPowered = false;
+    /*[HideInInspector]*/public bool isConnected = false;
 
     private new void Start()
     {
         base.Start();
         WorldManager.World.SetMachineAt(Coords, this);
     }
-
+    public void checkConnection()
+    {
+        foreach (PowerPole pole in poleManager.instance.powerPoles)
+        {
+                if (pole.CanConnectTo(this))
+                {
+                    pole.Connect(this);
+                }
+        }
+    }
     public ProcessingData GetProcessingData()
     {
         return new ProcessingData()
